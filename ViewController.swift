b89ad13d8cf5cb2d1ef0b09f10d2d9ecd4b4867a
TDFHAKR <alfie.bremer@icloud.com>
2025-02-27 16:49:31 +0000

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TPhaX
+//
+//  Created by Alfie Bremer on 26/02/2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
